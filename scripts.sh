055339746ccf777d59fd46dda7daa19e46323036
EC2 Default User <ec2-user@ip-172-31-24-149.ec2.internal>
2026-02-16 06:11:37 +0000

    initial linux_administration project

---
+++ scripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Print a greeting
+echo "Hello Atsu"
+
+# Print current date and time (no extra words)
+date
+
+# Print current user
+whoami
+
+# Print a message
+echo "This is my first Linux administration project!"
+
